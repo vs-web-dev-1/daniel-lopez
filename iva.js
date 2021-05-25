f29c490e369c9f690eb7709266539c34aa427a39
DanielLCoi <daniel.lopezc@softtek.com>
2021-05-25 12:28:18 +0200

    Corrección de error de cálculo de IVA

--- iva.js
+++ iva.js
@@ -1,8 +1,9 @@
-let iva = 21;
-let producto1 = {
-    precio : 6,
-    precioIva : this.precio * (1.00 + iva/100)
+const iva = 21;
+const producto1 = {
+    precio : 6.00,
+    
 }
+producto1.precioIva = producto1.precio * (1.00 + (iva/100));
 console.log ("Precio: " + producto1.precio + " €");
 console.log ("IVA: " + iva + " %");
 console.log ("Total: " + producto1.precioIva + " €");
